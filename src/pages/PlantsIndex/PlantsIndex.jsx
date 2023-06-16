630bbec607944a7848f92a8d664ea3e7d9601f9e
szaccagni <sarahzaccagni@gmail.com>
2023-06-16 11:39:33 -0400

    adds message for no inventory

--- src/pages/PlantsIndex/PlantsIndex.jsx
+++ src/pages/PlantsIndex/PlantsIndex.jsx
@@ -5,21 +5,31 @@ import './PlantsIndex.css'
 export default function PlantsIndex({userPlants, editInventory, setEditInventory, removePlant}) {
     return (
         <>
-            {editInventory && 
-                <div className='editing-link'>
-                    <Link to="/plants" onClick={() => setEditInventory(false)}>Done Editing</Link>
+            {
+            userPlants.length > 0 ? 
+                <>
+                {editInventory && 
+                    <div className='editing-link'>
+                        <Link to="/plants" onClick={() => setEditInventory(false)}>Done Editing</Link>
+                    </div>
+                }
+                <div className='your-plant-container'>
+                    {userPlants.map((plant, idx) => 
+                        <PlantCard 
+                            key={idx} 
+                            editInventory={editInventory}
+                            plant={plant} 
+                            removePlant={removePlant}
+                        />
+                    )}
+                </div>
+                </>
+                :
+                <div className='no-plants-msg'>
+                    <div>No plants have been added to your inventory yet, check out Browse All to add some!</div>
+                    <img src='/favicon.ico'></img>
                 </div>
             }
-            <div className='your-plant-container'>
-                {userPlants.map((plant, idx) => 
-                    <PlantCard 
-                        key={idx} 
-                        editInventory={editInventory}
-                        plant={plant} 
-                        removePlant={removePlant}
-                    />
-                )}
-            </div>
         </>
     )
 }
